e758380e0d288735387cd87eeba3846a1665226d
ro-el-c <chung3286@naver.com>
2023-12-22 21:57:44 +0900

    chore: 추후 작업을 위한 Controller 코드 추가

--- src/main/java/com/kuit/conet/controller/TeamController.java
+++ src/main/java/com/kuit/conet/controller/TeamController.java
@@ -29,4 +29,70 @@ public class TeamController {
         CreateTeamResponse response = teamService.createTeam(createTeamRequest, httpRequest, file);
         return new BaseResponse<CreateTeamResponse>(response);
     }
+
+/*    @PostMapping("/participate")
+    public BaseResponse<ParticipateTeamResponse> participateTeam(@RequestBody @Valid ParticipateTeamRequest participateRequest, HttpServletRequest httpRequest) {
+        ParticipateTeamResponse response = teamService.participateTeam(participateRequest, httpRequest);
+        return new BaseResponse<ParticipateTeamResponse>(response);
+    }
+
+    @PostMapping("/code")
+    public BaseResponse<RegenerateCodeResponse> regenerateCode(@RequestBody @Valid TeamIdRequest request) {
+        RegenerateCodeResponse response = teamService.regenerateCode(request);
+        return new BaseResponse<>(response);
+    }
+
+    @GetMapping
+    public BaseResponse<List<GetTeamResponse>> getTeam(HttpServletRequest httpRequest) {
+        List<GetTeamResponse> responses = teamService.getTeam(httpRequest);
+        return new BaseResponse<List<GetTeamResponse>>(responses);
+    }
+
+    @PostMapping("/leave")
+    public BaseResponse<String> leaveTeam(@RequestBody @Valid TeamIdRequest request, HttpServletRequest httpRequest) {
+        String response = teamService.leaveTeam(request, httpRequest);
+        return new BaseResponse<String>(response);
+    }
+
+    @PostMapping("/delete")
+    public BaseResponse<String> deleteTeam(@RequestBody @Valid TeamIdRequest request) {
+        String response = teamService.deleteTeam(request);
+        return new BaseResponse<String>(response);
+    }
+
+    @PostMapping("/update")
+    public BaseResponse<StorageImgResponse> updateTeam(@RequestPart(value = "request") @Valid UpdateTeamRequest updateTeamRequest, @RequestParam(value = "file") MultipartFile file) {
+        StorageImgResponse response = teamService.updateTeam(updateTeamRequest, file);
+        return new BaseResponse<StorageImgResponse>(response);
+    }
+
+    @GetMapping("/members")
+    public BaseResponse<List<GetTeamMemberResponse>> getTeamMembers(@ModelAttribute @Valid TeamIdRequest request) {
+        List<GetTeamMemberResponse> response = teamService.getTeamMembers(request);
+        return new BaseResponse<>(response);
+    }
+
+    @PostMapping("/bookmark")
+    public BaseResponse<String> bookmarkTeam(HttpServletRequest httpRequest, @RequestBody @Valid TeamIdRequest request) {
+        teamService.bookmarkTeam(httpRequest, request);
+        return new BaseResponse<>("모임을 즐겨찾기에 추가하였습니다.");
+    }
+
+    @PostMapping("/bookmark/delete")
+    public BaseResponse<String> unBookmarkTeam(HttpServletRequest httpRequest, @RequestBody @Valid TeamIdRequest request) {
+        teamService.unBookmarkTeam(httpRequest, request);
+        return new BaseResponse<>("모임을 즐겨찾기에서 삭제하였습니다.");
+    }
+
+    @GetMapping("/detail")
+    public BaseResponse<GetTeamResponse> getTeamDetail(HttpServletRequest httpRequest, @ModelAttribute @Valid TeamIdRequest request) {
+        GetTeamResponse response = teamService.getTeamDetail(httpRequest, request);
+        return new BaseResponse<>(response);
+    }
+
+    @GetMapping("/bookmark")
+    public BaseResponse<List<GetTeamResponse>> getBookmark(HttpServletRequest httpRequest) {
+        List<GetTeamResponse> responses = teamService.getBookmarks(httpRequest);
+        return new BaseResponse<>(responses);
+    }*/
 }
