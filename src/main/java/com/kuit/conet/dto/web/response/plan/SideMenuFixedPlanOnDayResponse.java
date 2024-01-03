fb0db61ece0e21dbbf7a107ef2a300abd765017a
ro-el <chung3286@naver.com>
2024-01-04 00:42:49 +0900

    style: delete unusable annotations

--- src/main/java/com/kuit/conet/dto/web/response/plan/SideMenuFixedPlanOnDayResponse.java
+++ src/main/java/com/kuit/conet/dto/web/response/plan/SideMenuFixedPlanOnDayResponse.java
@@ -1,15 +1,11 @@
 package com.kuit.conet.dto.web.response.plan;
 
-import com.kuit.conet.domain.plan.SideMenuFixedPlan;
 import lombok.*;
 
 import java.util.List;
 
 @Getter
-@Setter
-@NoArgsConstructor
 @AllArgsConstructor
-@ToString
 public class SideMenuFixedPlanOnDayResponse {
     int count;
     List<SideMenuFixedPlan> plans;
