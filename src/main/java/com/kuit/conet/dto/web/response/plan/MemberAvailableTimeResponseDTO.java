1eca11228a13146460ef1c512a2bc2d6b279ad70
ro-el-c <chung3286@naver.com>
2024-02-06 22:26:43 +0900

    feat: 대기 중인 약속의  구성원과 가능한 조회 api 관련 DTO 구현

---
+++ src/main/java/com/kuit/conet/dto/web/response/plan/MemberAvailableTimeResponseDTO.java
@@ -0,0 +1,32 @@
+package com.kuit.conet.dto.web.response.plan;
+
+import com.kuit.conet.dto.plan.MemberDateTimeDTO;
+import com.kuit.conet.jpa.domain.plan.Plan;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.sql.Date;
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@AllArgsConstructor
+public class MemberAvailableTimeResponseDTO {
+    private Long teamId;
+    private Long planId;
+    private String planName;
+    private Date planStartPeriod;
+    private Date planEndPeriod;
+    private Map<Integer, Long> endNumberForEachSection; // 각 구간의 최대 인원 수
+    private List<MemberDateTimeDTO> availableMemberDateTime;
+
+    public MemberAvailableTimeResponseDTO(Long teamId, Plan plan, Map<Integer, Long> endNumberForEachSection, List<MemberDateTimeDTO> availableMemberDateTime) {
+        this.teamId = teamId;
+        this.planId = plan.getId();
+        this.planName = plan.getName();
+        this.planStartPeriod = plan.getStartPeriod();
+        this.planEndPeriod = plan.getEndPeriod();
+        this.endNumberForEachSection = endNumberForEachSection;
+        this.availableMemberDateTime = availableMemberDateTime;
+    }
+}
