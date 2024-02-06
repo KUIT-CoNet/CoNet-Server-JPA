9339f98e3ed401f8ce2aab603b293312ecded2dc
ro-el-c <chung3286@naver.com>
2024-02-06 22:33:10 +0900

    feat: 1~3구간의 최대 인원수 계산 메서드 구현
    각 구간의 최대 인원, 즉, 다음 구간과의 경계가 되는 인원수를 map 구조로 반환

--- src/main/java/com/kuit/conet/jpa/service/PlanService.java
+++ src/main/java/com/kuit/conet/jpa/service/PlanService.java
@@ -3,6 +3,9 @@ package com.kuit.conet.jpa.service;
 
 import com.kuit.conet.dto.plan.*;
 import com.kuit.conet.dto.web.request.plan.*;
+import com.kuit.conet.dto.plan.AvailableMemberDTO;
+import com.kuit.conet.dto.plan.MemberAvailableTimeDTO;
+import com.kuit.conet.dto.plan.MemberDateTimeDTO;
 import com.kuit.conet.dto.web.response.plan.*;
 import com.kuit.conet.jpa.domain.plan.*;
 import com.kuit.conet.jpa.domain.team.Team;
@@ -15,9 +18,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.sql.Date;
 import java.sql.Time;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 import static com.kuit.conet.jpa.service.validator.PlanValidator.*;
 import static com.kuit.conet.utils.DateAndTimeFormatter.*;
@@ -29,8 +30,18 @@ import static com.kuit.conet.utils.DateAndTimeFormatter.*;
 public class PlanService {
     private final MemberRepository memberRepository;
     private final TeamRepository teamRepository;
+    private final TeamMemberRepository teamMemberRepository;
     private final PlanRepository planRepository;
     private final PlanMemberTimeRepository planMemberTimeRepository;
+
+    private final static int ONE_WEEK_DAYS = 7;
+    private final static int ONE_DAY_HOURS = 24;
+    private final static int MIN_TIME_NUMBER = 0;
+    private final static int MAX_TIME_NUMBER = 23;
+    private final static String AVAILABLE_TIME_SPLIT_REGEX = ",";
+    private final static int SECTION_DIVISOR = 3;
+    private final static String SECTION = "section";
+    private final static String INTERVAL_NUMBER = "intervalEndNumberForEachSection";
 
     public CreatePlanResponseDTO createPlan(CreatePlanRequestDTO planRequest) {
         Date endDate = setEndDate(planRequest.getPlanStartDate());
@@ -166,4 +177,38 @@ public class PlanService {
                 }).toList();
     }
 
+    public MemberAvailableTimeResponseDTO getAvailableTimeSlot(Long planId) {
+        Plan plan = planRepository.findById(planId);
+
+        // 대기 중인 약속일 때만 구성원의 가능한 시간 조회
+        validatePlanIsWaiting(plan);
+
+        Date periodStartDate = plan.getStartPeriod();
+
+        //모임 구성원 수 -> 구간 나눌 때 필요
+        Long teamId = plan.getTeam().getId();
+        Long teamMemberTotalCount = teamMemberRepository.getCount(teamId);
+
+        //구간별 최대 인원수 구하기
+        Map<Integer, Long> endNumberForEachSection = getIntervalEndBySection(teamMemberTotalCount);
+
+        return new MemberAvailableTimeResponseDTO();
+    }
+
+    private static Map<Integer, Long> getIntervalEndBySection(Long totalMemberCount) {
+        Map<Integer, Long> endNumberForEachSection = new HashMap<>();
+
+        //기본 구간 크기와 나머지 계산
+        Long baseIntervalSize = totalMemberCount / SECTION_DIVISOR;
+        Long remainder = totalMemberCount % SECTION_DIVISOR;
+
+        //3구간의 범위가 너무 크지 않게, 비교적 균등하게 범위가 나뉘도록 하기 위하여
+        Long firstIntervalEnd = baseIntervalSize + (remainder > 0 ? 1 : 0);
+        Long secondIntervalEnd = firstIntervalEnd + baseIntervalSize + (remainder > 1 ? 1 : 0);
+        endNumberForEachSection.put(1, firstIntervalEnd);
+        endNumberForEachSection.put(2, secondIntervalEnd);
+        endNumberForEachSection.put(3, totalMemberCount);
+
+        return endNumberForEachSection;
+    }
 }
