b009c3bd2bc5ddec8cf7bb25b83ed9090351003b
kang yeon joo <rkdduswndi@naver.com>
2024-02-07 18:25:33 +0900

    feat: Notice 엔티티 생성

---
+++ src/main/java/com/kuit/conet/jpa/domain/notice/Notice.java
@@ -0,0 +1,24 @@
+package com.kuit.conet.jpa.domain.notice;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Notice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "notice_id")
+    private Long id;
+
+    private String title;
+
+    @Lob
+    private String content;
+
+    private LocalDate date;
+}
