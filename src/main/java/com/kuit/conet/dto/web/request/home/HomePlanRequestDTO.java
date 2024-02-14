2fb89fbb91383974c5a99c6d19dc6d3eb3c1a8c4
정소민 <yjk70702002@gmail.com>
2024-02-15 02:12:06 +0900

    fix: HomePlanRequestDTO에 @Setter 제거

--- src/main/java/com/kuit/conet/dto/web/request/home/HomePlanRequestDTO.java
+++ src/main/java/com/kuit/conet/dto/web/request/home/HomePlanRequestDTO.java
@@ -1,12 +1,8 @@
 package com.kuit.conet.dto.web.request.home;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
-@AllArgsConstructor
 public class HomePlanRequestDTO {
     private String searchDate;
     // 특정 달의 조회: "yyyy-MM"
