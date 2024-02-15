807e02e8f7ced33b98be0eaca65d84f9e9ede0a0
정소민 <yjk70702002@gmail.com>
2024-02-15 18:50:19 +0900

    refactor: BearerAuthInterceptor log debug로 변경

--- src/main/java/com/kuit/conet/config/UserIdResolver.java
+++ src/main/java/com/kuit/conet/config/UserIdResolver.java
@@ -2,6 +2,7 @@ package com.kuit.conet.config;
 
 import com.kuit.conet.annotation.UserId;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
@@ -9,6 +10,7 @@ import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+@Slf4j
 @Component
 public class UserIdResolver implements HandlerMethodArgumentResolver {
     //Interceptor 이후 동작
@@ -22,6 +24,7 @@ public class UserIdResolver implements HandlerMethodArgumentResolver {
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
                                   NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
         HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        log.debug("UserIdResolver userId: {}", request.getAttribute("userId"));
         return request.getAttribute("userId");
     }
 }
