3d193b224864a445708c00d600e940d221f99dfb
ro-el <chung3286@naver.com>
2024-02-16 05:14:26 +0900

    feat: validator에 log 추가

--- src/main/java/com/kuit/conet/jpa/service/validator/AuthValidator.java
+++ src/main/java/com/kuit/conet/jpa/service/validator/AuthValidator.java
@@ -3,19 +3,20 @@ package com.kuit.conet.jpa.service.validator;
 import com.kuit.conet.common.exception.InvalidTokenException;
 import lombok.extern.slf4j.Slf4j;
 
-import static com.kuit.conet.common.response.status.BaseExceptionResponseStatus.INVALID_REFRESH_TOKEN;
-import static com.kuit.conet.common.response.status.BaseExceptionResponseStatus.IP_MISMATCH;
+import static com.kuit.conet.common.response.status.BaseExceptionResponseStatus.*;
 
 @Slf4j
 public class AuthValidator {
     public static void validateRefreshTokenExisting(String existingIp) {
         if (existingIp == null) {
+            log.error(INVALID_REFRESH_TOKEN.getMessage());
             throw new InvalidTokenException(INVALID_REFRESH_TOKEN);
         }
     }
 
     public static void compareClientIpFromRedis(String existingIp, String clientIp) {
         if (!existingIp.equals(clientIp)) {
+            log.error(IP_MISMATCH.getMessage());
             throw new InvalidTokenException(IP_MISMATCH);
         }
     }
